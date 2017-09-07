accb85665f197b86ed5db465bb636d6972ae2c28
Runzhe Yang <runzheyang@icloud.com>
2017-09-06 22:02:46 -0400

    resize the figures

--- results/replot/replot.py
+++ results/replot/replot.py
@@ -6,17 +6,25 @@ import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
 sns.set(style='ticks', palette='Set2')
 
+who = "random"
+# who = "weak"
+# who = "median"
+# who = "strong"
+
 def plot_curve_and_reward(curves, rewards, gammas, student_types):
-	fig = plt.figure()
+	fig = plt.figure(figsize=(8,6))
+	# fig = plt.figure()
 	ax1 = fig.add_subplot(111)
+	plt.tick_params(
+		axis='x',
+		which='both',
+		top='off')
 	ax1 = sns.pointplot(x="round",
 						y="value",
 						hue="type",
 						data=curves_sum,
-						ms=14,
-						markeredgewidth=5,
+						scale=1.5,
 						markers=["o", "v", "s", "x"],
-						markerfacecolor="None",
 						linestyles=["-", "--", "-.", ":"])
 	ax2 = ax1.twinx()
 	ax2 = sns.barplot(x="round",
@@ -24,15 +32,21 @@ def plot_curve_and_reward(curves, rewards, gammas, student_types):
 					  hue="type", 
 					  palette="Reds_r", 
 					  data=rewards)
-	# ax1.set_ylim(top=1.1)
+	ax1.set(ylim=(0.58, 1.05))
 	ax2.set(ylim=(0.0, 3))
-	ax1.set_xlabel("round")
-	ax1.set_ylabel("performance")
-	ax2.set_ylabel("reward")
-	ax1.legend(loc="upper left")
-	ax2.legend(loc="upper right")
+	ax1.set_xlabel("round",  fontsize=20)
+	ax1.set_ylabel("performance", fontsize=20)
+	ax2.set_ylabel("reward", fontsize=20)
+	ax1.legend(loc="upper left", fontsize=16)
+	ax2.legend(loc="upper right", fontsize=16)
+	for label in ax1.xaxis.get_ticklabels():
+		label.set_fontsize(14)
+	for label in ax1.yaxis.get_ticklabels():
+		label.set_fontsize(14)	
+	for label in ax2.yaxis.get_ticklabels():
+		label.set_fontsize(14)	
 	pp = PdfPages("rp-gamma-(%0.2f)_type-%s.pdf"%(gammas, student_types))
-	fig.savefig(pp, format='pdf')
+	fig.savefig(pp, format='pdf', bbox_inches='tight')
 	pp.close()
 
 file = open("log.json")
@@ -47,7 +61,7 @@ rewards = {"round": [], "type": [], "value": []}
 
 # get corresponding records
 for rec in records:
-	if rec["gamma"] == 0.7 and rec["stu"] == "random":
+	if rec["gamma"] == 0.7 and rec["stu"] == who:
 		if rec["rnd"] == 0:
 			init_perf = rec["real"]
 		curves["round"].append(rec["rnd"])
@@ -76,6 +90,6 @@ curves_sum["type"]  = curves["type"]  + model_curves["type"]
 curves_sum["value"] = curves["value"] + model_curves["value"]
 
 
-plot_curve_and_reward(curves_sum, rewards, 0.7, "random")
+plot_curve_and_reward(curves_sum, rewards, 0.7, who)
 
 
